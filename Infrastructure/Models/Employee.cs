0d29404f0fe8725cb17a61792e279f0be3420be9
Serbez Konstantin <kserbez.work@gmail.com>
2020-07-23 21:12:40 +0300

    Add project files.

---
+++ Infrastructure/Models/Employee.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Infrastructure.Models
+{
+    public class Employee
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Surname { get; set; }
+        public Position Position { get; set; }
+
+    }
+}
