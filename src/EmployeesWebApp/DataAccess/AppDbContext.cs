d71620e8149054ed05b2c0b46747d970c69d5cad
Serbez Konstantin <kserbez.work@gmail.com>
2020-07-27 00:04:40 +0300

    moved project to src folder

---
+++ src/EmployeesWebApp/DataAccess/AppDbContext.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Microsoft.EntityFrameworkCore;
+
+using EmployeesWebApp.Entities;
+
+namespace Infrastructure.DataAccess
+{
+    public class AppDbContext : DbContext
+    {
+        public AppDbContext(DbContextOptions options) : base(options) { }
+
+        public DbSet<Employee> Employees { get; set; }
+        public DbSet<Position> Positions { get; set; }
+
+    }
+}
---
+++ src/EmployeesWebApp/Entities/Position.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace EmployeesWebApp.Entities
+{
+    public class Position
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+
+    }
+}
