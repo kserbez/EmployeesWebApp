7fbd7f7bff7f68b55f28ab0be57ba8010a02f51d
Serbez Konstantin <kserbez.work@gmail.com>
2020-07-27 05:14:56 +0300

    in progress

--- src/Web/Controllers/Api/EmployeeController.cs
+++ src/Web/Controllers/Api/EmployeeController.cs
@@ -4,6 +4,7 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using Web.Entities;
 
 namespace Web.Controllers.Api
 {
@@ -17,28 +18,29 @@ namespace Web.Controllers.Api
             _db = db;
         }
 
-        //[HttpPost]
-        //public async Task<BiblObject> SaveData([FromBody] BiblObject obj)
-        //{
-        //    Citation citation = new Citation();
-        //    try
-        //    {
-        //        //insert to DB to table Citation new citate
-        //        citation.allData = obj.otherData;
-        //        citation.idUser = _userManager.GetUserId(User);
-        //        citation.date = DateTime.Now;
-        //        _db.Citation.Add(citation);
+        [HttpPost]
+        public async Task<Employee> SaveData([FromBody] Employee employee)
+        {
+            //Citation citation = new Citation();
+            //try
+            //{
+            //    //insert to DB to table Citation new citate
+            //    citation.allData = obj.otherData;
+            //    citation.idUser = _userManager.GetUserId(User);
+            //    citation.date = DateTime.Now;
+            //    _db.Citation.Add(citation);
 
-        //        await _db.SaveChangesAsync();
-        //        //call method for add author of citate
-        //        obj.authors = await SaveAuthorsForBibl(obj.authors, citation.id);
-        //        obj.date = citation.date;
-        //        obj.id = citation.id;
-        //    }
-        //    catch (Exception ex)
-        //    { obj.exeption = ex; }
-        //    return obj;
-        //}
+            //    await _db.SaveChangesAsync();
+            //    //call method for add author of citate
+            //    obj.authors = await SaveAuthorsForBibl(obj.authors, citation.id);
+            //    obj.date = citation.date;
+            //    obj.id = citation.id;
+            //}
+            //catch (Exception ex)
+            //{ obj.exeption = ex; }
+            
+            return employee;
+        }
 
 
     }
--- src/Web/Entities/Employee.cs
+++ src/Web/Entities/Employee.cs
@@ -1,19 +1,21 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.ComponentModel.DataAnnotations;
+using System.Runtime.Serialization;
 using System.Text;
 
 namespace Web.Entities
 {
+    [DataContract]
     public class Employee
     {
         [Key]
         public int Id { get; set; }
 
-        [StringLength(100)]
+        [StringLength(100), DataMember(Name = "name")]
         public string Name { get; set; }
 
-        [StringLength(100), Required(ErrorMessage = "Employee surname is required!")]
+        [StringLength(100), Required(ErrorMessage = "Employee surname is required!"), DataMember(Name = "surname")]
         public string Surname { get; set; }
 
     }
