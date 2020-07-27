ecb7e93265d8c0cb6b53e6f89e86c7a4423cc1ab
Serbez Konstantin <kserbez.work@gmail.com>
2020-07-27 04:05:52 +0300

    fix

--- src/Web/Controllers/Api/EmployeeController.cs
+++ src/Web/Controllers/Api/EmployeeController.cs
@@ -17,28 +17,28 @@ namespace Web.Controllers.Api
             _db = db;
         }
 
-        [HttpPost]
-        public async Task<BiblObject> SaveData([FromBody] BiblObject obj)
-        {
-            Citation citation = new Citation();
-            try
-            {
-                //insert to DB to table Citation new citate
-                citation.allData = obj.otherData;
-                citation.idUser = _userManager.GetUserId(User);
-                citation.date = DateTime.Now;
-                _db.Citation.Add(citation);
+        //[HttpPost]
+        //public async Task<BiblObject> SaveData([FromBody] BiblObject obj)
+        //{
+        //    Citation citation = new Citation();
+        //    try
+        //    {
+        //        //insert to DB to table Citation new citate
+        //        citation.allData = obj.otherData;
+        //        citation.idUser = _userManager.GetUserId(User);
+        //        citation.date = DateTime.Now;
+        //        _db.Citation.Add(citation);
 
-                await _db.SaveChangesAsync();
-                //call method for add author of citate
-                obj.authors = await SaveAuthorsForBibl(obj.authors, citation.id);
-                obj.date = citation.date;
-                obj.id = citation.id;
-            }
-            catch (Exception ex)
-            { obj.exeption = ex; }
-            return obj;
-        }
+        //        await _db.SaveChangesAsync();
+        //        //call method for add author of citate
+        //        obj.authors = await SaveAuthorsForBibl(obj.authors, citation.id);
+        //        obj.date = citation.date;
+        //        obj.id = citation.id;
+        //    }
+        //    catch (Exception ex)
+        //    { obj.exeption = ex; }
+        //    return obj;
+        //}
 
 
     }
