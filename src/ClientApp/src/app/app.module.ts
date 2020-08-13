b719612dcac96a008bff93f2ad5097f492df6375
Kostia Serbez <kserbez.work@gmail.com>
2020-08-13 19:05:21 +0300

    added material

--- src/ClientApp/src/app/app.module.ts
+++ src/ClientApp/src/app/app.module.ts
@@ -5,10 +5,14 @@ import { HttpClientModule, HTTP_INTERCEPTORS } from '@angular/common/http';
 import { RouterModule } from '@angular/router';
 
 import { AppComponent } from './app.component';
-import { NavMenuComponent } from './nav-menu/nav-menu.component';
-import { HomeComponent } from './home/home.component';
-import { CounterComponent } from './counter/counter.component';
-import { FetchDataComponent } from './fetch-data/fetch-data.component';
+import { NavMenuComponent } from './components/nav-menu/nav-menu.component';
+import { HomeComponent } from './components/home/home.component';
+import { CounterComponent } from './components/counter/counter.component';
+import { FetchDataComponent } from './components/fetch-data/fetch-data.component';
+import { NoopAnimationsModule } from '@angular/platform-browser/animations';
+
+import { DialogOverviewExample } from './components/dialog-overview-example/dialog-overview-example';
+import { MaterialModule } from './material.module';
 
 @NgModule({
   declarations: [
@@ -16,7 +20,10 @@ import { FetchDataComponent } from './fetch-data/fetch-data.component';
     NavMenuComponent,
     HomeComponent,
     CounterComponent,
-    FetchDataComponent
+    FetchDataComponent,
+
+    DialogOverviewExample,
+    MaterialModule,
   ],
   imports: [
     BrowserModule.withServerTransition({ appId: 'ng-cli-universal' }),
@@ -26,9 +33,15 @@ import { FetchDataComponent } from './fetch-data/fetch-data.component';
       { path: '', component: HomeComponent, pathMatch: 'full' },
       { path: 'counter', component: CounterComponent },
       { path: 'fetch-data', component: FetchDataComponent },
-    ])
+      // { path: 'dialog-example', component: DialogOverviewExample },
+    ]),
+    NoopAnimationsModule,
   ],
-  providers: [],
-  bootstrap: [AppComponent]
+  providers: [
+    // { provide: MAT_DIALOG_DEFAULT_OPTIONS, useValue: {hasBackdrop: false} }
+  ],
+  bootstrap: [
+    AppComponent
+  ]
 })
 export class AppModule { }
---
+++ src/ClientApp/src/app/components/dialog-overview-example/dialog-overview-example.ts
@@ -0,0 +1,31 @@
+import { Component } from '@angular/core';
+import { MatDialog } from '@angular/material/dialog';
+import { DialogOverviewExampleDialog } from '../dialog-overview-example-dialog/dialog-overview-example-dialog';
+
+/**
+ * @title Dialog Overview
+ */
+@Component({
+  selector: 'dialog-overview-example',
+  templateUrl: 'dialog-overview-example.html',
+  styleUrls: ['dialog-overview-example.scss'],
+})
+export class DialogOverviewExample {
+  animal: string;
+  name: string;
+
+  constructor(public dialog: MatDialog) {}
+
+  openDialog(): void {
+    const dialogRef = this.dialog.open(DialogOverviewExampleDialog, {
+      width: '250px',
+      data: {name: this.name, animal: this.animal}
+    });
+
+    dialogRef.afterClosed().subscribe(result => {
+      console.log('The dialog was closed');
+      this.animal = result;
+    });
+  }
+
+}
